a7fe6d825d2a8a46838173d01913210830a24beb
Harley Laue <losinggeneration@gmail.com>
2016-06-19 17:05:54 -0700

    Add top level GeoJSON Marshal/Unmarshalling

--- geojson.go
+++ geojson.go
@@ -1,7 +1,63 @@
 package geojson
 
+import (
+	"encoding/json"
+	"errors"
+)
+
+var ErrInvalidGeoJSON = errors.New("invalid GeoJSON to unmarshal")
+
 type Object struct {
 	Type        string       `json:"type"`
 	BoundingBox *BoundingBox `json:"bbox,omitempty"`
 	CRS         *CRS         `json:"crs,omitempty"`
+}
+
+type GeoJSON struct {
+	Object
+	*Geometry
+	*Feature
+	*FeatureCollection
+}
+
+func (g GeoJSON) MarshalJSON() ([]byte, error) {
+	if g.Geometry != nil {
+		return json.Marshal(g.Geometry)
+	}
+	if g.Feature != nil {
+		return json.Marshal(g.Feature)
+	}
+	if g.FeatureCollection != nil {
+		return json.Marshal(g.FeatureCollection)
+	}
+
+	return []byte("null"), nil
+}
+
+func (g *GeoJSON) UnmarshalJSON(b []byte) error {
+	// Minimum amount to unmarshal on first pass
+	var t struct {
+		Type string `json:"type"`
+	}
+
+	if err := json.Unmarshal(b, &t); err != nil {
+		return err
+	}
+
+	switch t.Type {
+	case "Point", "MultiPoint",
+		"LineString", "MultiLineString",
+		"Polygon", "MultiPolygon",
+		"GeometryCollection":
+		g.Geometry = new(Geometry)
+		return json.Unmarshal(b, g.Geometry)
+	case "Feature":
+		g.Feature = new(Feature)
+		return json.Unmarshal(b, g.Feature)
+	case "FeatureCollection":
+		g.Feature = new(Feature)
+		return json.Unmarshal(b, g.Feature)
+	}
+
+	return ErrInvalidGeoJSON
 }
--- geojson_test.go
+++ geojson_test.go
@@ -1,9 +1,96 @@
 package geojson
 
-import "regexp"
+import (
+	"encoding/json"
+	"regexp"
+	"testing"
+)
 
 var r *regexp.Regexp
 
 func init() {
 	r = regexp.MustCompile(`\s`)
+}
+
+func equalGeoJSON(g1, g2 GeoJSON, t *testing.T) {
+	equalGeometries(g1.Geometry, g2.Geometry, t)
+	equalFeatures(g1.Feature, g2.Feature, t)
+	equalFeatureCollections(g1.FeatureCollection, g2.FeatureCollection, t)
+}
+
+func TestMarshalRealGeoJSON(t *testing.T) {
+	o := Object{
+		Type: "Feature",
+	}
+
+	g := GeoJSON{
+		Object: o,
+		Feature: &Feature{
+			Object: o,
+			Geometry: &Geometry{
+				Point: &Point{
+					Coordinates: Coordinate{125.6, 10.1},
+				},
+			},
+			Properties: map[string]interface{}{
+				"name": "Dinagat Islands",
+			},
+		},
+	}
+
+	expected := []byte(r.ReplaceAllString(`{
+			"type": "Feature",
+			"geometry": {
+				"type": "Point",
+				"coordinates": [125.6, 10.1]
+			},
+			"properties": {
+				"name": "Dinagat Islands"
+			}
+		}`, ""))
+
+	if actual, err := json.Marshal(g); err != nil {
+		t.Errorf("expected nil but got %q", err)
+	} else if r.ReplaceAllString(string(actual), "") != string(expected) {
+		t.Errorf("expected %q but got %q", string(expected), string(actual))
+	}
+
+}
+
+func TestUnmarshalRealGeoJSON(t *testing.T) {
+	o := Object{
+		Type: "Feature",
+	}
+	expected := GeoJSON{
+		Object: o,
+		Feature: &Feature{
+			Object: o,
+			Geometry: &Geometry{
+				Point: &Point{
+					Coordinates: Coordinate{125.6, 10.1},
+				},
+			},
+			Properties: map[string]interface{}{
+				"name": "Dinagat Islands",
+			},
+		},
+	}
+
+	j := []byte(`{
+			"type": "Feature",
+			"geometry": {
+				"type": "Point",
+				"coordinates": [125.6, 10.1]
+			},
+			"properties": {
+				"name": "Dinagat Islands"
+			}
+		}`)
+
+	g := GeoJSON{}
+	if err := json.Unmarshal(j, &g); err != nil {
+		t.Errorf("expected nil but got %q", err)
+	} else {
+		equalGeoJSON(expected, g, t)
+	}
 }
